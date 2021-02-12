e2fa5d82605edfe847cd24eb5ebdc1540136ad6f
Kloeber-dev <71834748+Kloeber-dev@users.noreply.github.com>
2021-02-12 17:01:08 -0300

    trocando o banco
    troquei pelo banco novo

--- terralis.sql
+++ terralis.sql
@@ -1,264 +1,242 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
 --
--- Host: 127.0.0.1
--- Tempo de geração: 04-Fev-2021 às 19:54
--- Versão do servidor: 10.4.17-MariaDB
--- versão do PHP: 8.0.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: terralis
+-- ------------------------------------------------------
+-- Server version	8.0.23
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Banco de dados: `terralis`
+-- Table structure for table `alternativa`
 --
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `alternativa`
---
-
+DROP TABLE IF EXISTS `alternativa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `alternativa` (
-  `id_alternativa` int(11) NOT NULL,
+  `id_alternativa` int NOT NULL AUTO_INCREMENT,
   `texto` blob NOT NULL,
   `veracidade` tinyint(1) NOT NULL,
-  `id_questao` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `id_questao` int NOT NULL,
+  PRIMARY KEY (`id_alternativa`),
+  KEY `id_questao` (`id_questao`),
+  CONSTRAINT `alternativa_ibfk_1` FOREIGN KEY (`id_questao`) REFERENCES `questao` (`id_questao`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura da tabela `desempenho`
+-- Dumping data for table `alternativa`
 --
 
+LOCK TABLES `alternativa` WRITE;
+/*!40000 ALTER TABLE `alternativa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alternativa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `desempenho`
+--
+
+DROP TABLE IF EXISTS `desempenho`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `desempenho` (
-  `id_usuario` int(11) NOT NULL,
-  `id_questionario` int(5) NOT NULL,
-  `qtd_questao_correta` int(5) NOT NULL,
-  `qtd_questao_errada` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `id_usuario` int NOT NULL,
+  `id_questionario` int NOT NULL,
+  `qtd_questao_correta` int NOT NULL,
+  `qtd_questao_errada` int NOT NULL,
+  PRIMARY KEY (`id_usuario`,`id_questionario`),
+  KEY `id_questionario` (`id_questionario`),
+  CONSTRAINT `desempenho_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  CONSTRAINT `desempenho_ibfk_2` FOREIGN KEY (`id_questionario`) REFERENCES `questionario` (`id_questionario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura da tabela `disciplina`
+-- Dumping data for table `desempenho`
 --
 
+LOCK TABLES `desempenho` WRITE;
+/*!40000 ALTER TABLE `desempenho` DISABLE KEYS */;
+/*!40000 ALTER TABLE `desempenho` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `disciplina`
+--
+
+DROP TABLE IF EXISTS `disciplina`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `disciplina` (
-  `id_disciplina` int(11) NOT NULL,
-  `nome_disciplina` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `id_disciplina` int NOT NULL AUTO_INCREMENT,
+  `nome_disciplina` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_disciplina`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura da tabela `questao`
+-- Dumping data for table `disciplina`
 --
 
+LOCK TABLES `disciplina` WRITE;
+/*!40000 ALTER TABLE `disciplina` DISABLE KEYS */;
+/*!40000 ALTER TABLE `disciplina` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questao`
+--
+
+DROP TABLE IF EXISTS `questao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `questao` (
-  `id_questao` int(11) NOT NULL,
-  `enunciado` blob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `id_questao` int NOT NULL AUTO_INCREMENT,
+  `enunciado` blob NOT NULL,
+  PRIMARY KEY (`id_questao`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura da tabela `questionario`
+-- Dumping data for table `questao`
 --
 
+LOCK TABLES `questao` WRITE;
+/*!40000 ALTER TABLE `questao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `questao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questionario`
+--
+
+DROP TABLE IF EXISTS `questionario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `questionario` (
-  `id_questionario` int(11) NOT NULL,
-  `num_questoes` int(5) NOT NULL,
-  `id_tema` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `id_questionario` int NOT NULL AUTO_INCREMENT,
+  `num_questoes` int NOT NULL,
+  `id_tema` int NOT NULL,
+  PRIMARY KEY (`id_questionario`),
+  KEY `id_tema` (`id_tema`),
+  CONSTRAINT `questionario_ibfk_1` FOREIGN KEY (`id_tema`) REFERENCES `tema` (`id_tema`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura da tabela `questionario_questao`
+-- Dumping data for table `questionario`
 --
 
+LOCK TABLES `questionario` WRITE;
+/*!40000 ALTER TABLE `questionario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `questionario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questionario_questao`
+--
+
+DROP TABLE IF EXISTS `questionario_questao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `questionario_questao` (
-  `id_questao` int(11) NOT NULL,
-  `id_questionario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `id_questao` int NOT NULL,
+  `id_questionario` int NOT NULL,
+  PRIMARY KEY (`id_questao`,`id_questionario`),
+  KEY `id_questionario` (`id_questionario`),
+  CONSTRAINT `questionario_questao_ibfk_1` FOREIGN KEY (`id_questao`) REFERENCES `questao` (`id_questao`),
+  CONSTRAINT `questionario_questao_ibfk_2` FOREIGN KEY (`id_questionario`) REFERENCES `questionario` (`id_questionario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura da tabela `tema`
+-- Dumping data for table `questionario_questao`
 --
 
+LOCK TABLES `questionario_questao` WRITE;
+/*!40000 ALTER TABLE `questionario_questao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `questionario_questao` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tema`
+--
+
+DROP TABLE IF EXISTS `tema`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tema` (
-  `id_tema` int(11) NOT NULL,
+  `id_tema` int NOT NULL AUTO_INCREMENT,
   `link_video` blob NOT NULL,
   `assunto` varchar(255) NOT NULL,
   `resumo` blob NOT NULL,
-  `id_disciplina` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  `id_disciplina` int NOT NULL,
+  PRIMARY KEY (`id_tema`),
+  KEY `id_disciplina` (`id_disciplina`),
+  CONSTRAINT `tema_ibfk_1` FOREIGN KEY (`id_disciplina`) REFERENCES `disciplina` (`id_disciplina`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estrutura da tabela `usuario`
+-- Dumping data for table `tema`
 --
 
+LOCK TABLES `tema` WRITE;
+/*!40000 ALTER TABLE `tema` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tema` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
-  `id_usuario` int(11) NOT NULL,
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
   `sobrenome` varchar(255) NOT NULL,
   `data_nascimento` date NOT NULL,
-  `senha` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `senha` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Índices para tabelas despejadas
+-- Dumping data for table `usuario`
 --
 
---
--- Índices para tabela `alternativa`
---
-ALTER TABLE `alternativa`
-  ADD PRIMARY KEY (`id_alternativa`),
-  ADD KEY `id_questao` (`id_questao`);
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Índices para tabela `desempenho`
---
-ALTER TABLE `desempenho`
-  ADD PRIMARY KEY (`id_usuario`,`id_questionario`),
-  ADD KEY `id_questionario` (`id_questionario`);
-
---
--- Índices para tabela `disciplina`
---
-ALTER TABLE `disciplina`
-  ADD PRIMARY KEY (`id_disciplina`);
-
---
--- Índices para tabela `questao`
---
-ALTER TABLE `questao`
-  ADD PRIMARY KEY (`id_questao`);
-
---
--- Índices para tabela `questionario`
---
-ALTER TABLE `questionario`
-  ADD PRIMARY KEY (`id_questionario`),
-  ADD KEY `id_tema` (`id_tema`);
-
---
--- Índices para tabela `questionario_questao`
---
-ALTER TABLE `questionario_questao`
-  ADD PRIMARY KEY (`id_questao`,`id_questionario`),
-  ADD KEY `id_questionario` (`id_questionario`);
-
---
--- Índices para tabela `tema`
---
-ALTER TABLE `tema`
-  ADD PRIMARY KEY (`id_tema`),
-  ADD KEY `id_disciplina` (`id_disciplina`);
-
---
--- Índices para tabela `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
+-- Dumping events for database 'terralis'
 --
 
 --
--- AUTO_INCREMENT de tabela `alternativa`
+-- Dumping routines for database 'terralis'
 --
-ALTER TABLE `alternativa`
-  MODIFY `id_alternativa` int(11) NOT NULL AUTO_INCREMENT;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- AUTO_INCREMENT de tabela `disciplina`
---
-ALTER TABLE `disciplina`
-  MODIFY `id_disciplina` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `questao`
---
-ALTER TABLE `questao`
-  MODIFY `id_questao` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `questionario`
---
-ALTER TABLE `questionario`
-  MODIFY `id_questionario` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `tema`
---
-ALTER TABLE `tema`
-  MODIFY `id_tema` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restrições para despejos de tabelas
---
-
---
--- Limitadores para a tabela `alternativa`
---
-ALTER TABLE `alternativa`
-  ADD CONSTRAINT `alternativa_ibfk_1` FOREIGN KEY (`id_questao`) REFERENCES `questao` (`id_questao`);
-
---
--- Limitadores para a tabela `desempenho`
---
-ALTER TABLE `desempenho`
-  ADD CONSTRAINT `desempenho_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
-  ADD CONSTRAINT `desempenho_ibfk_2` FOREIGN KEY (`id_questionario`) REFERENCES `questionario` (`id_questionario`),
-  ADD CONSTRAINT `desempenho_ibfk_3` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
-
---
--- Limitadores para a tabela `questionario`
---
-ALTER TABLE `questionario`
-  ADD CONSTRAINT `questionario_ibfk_1` FOREIGN KEY (`id_tema`) REFERENCES `tema` (`id_tema`);
-
---
--- Limitadores para a tabela `questionario_questao`
---
-ALTER TABLE `questionario_questao`
-  ADD CONSTRAINT `questionario_questao_ibfk_1` FOREIGN KEY (`id_questao`) REFERENCES `questao` (`id_questao`),
-  ADD CONSTRAINT `questionario_questao_ibfk_2` FOREIGN KEY (`id_questionario`) REFERENCES `questionario` (`id_questionario`),
-  ADD CONSTRAINT `questionario_questao_ibfk_3` FOREIGN KEY (`id_questao`) REFERENCES `questao` (`id_questao`),
-  ADD CONSTRAINT `questionario_questao_ibfk_4` FOREIGN KEY (`id_questao`) REFERENCES `questao` (`id_questao`),
-  ADD CONSTRAINT `questionario_questao_ibfk_5` FOREIGN KEY (`id_questao`) REFERENCES `questao` (`id_questao`),
-  ADD CONSTRAINT `questionario_questao_ibfk_6` FOREIGN KEY (`id_questao`) REFERENCES `questao` (`id_questao`);
-
---
--- Limitadores para a tabela `tema`
---
-ALTER TABLE `tema`
-  ADD CONSTRAINT `tema_ibfk_1` FOREIGN KEY (`id_disciplina`) REFERENCES `disciplina` (`id_disciplina`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-02-12 16:55:05
